a914b2e0b283d29deed0a65e00977a5e3fb9192b
nkwaaaa <nkwaaaa@gmail.com>
2023-05-31 10:43:25 -0300

    add trpc base config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express from "express"
 import morgan from "morgan"
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { router, createContext } from "./trpc"
 
 const app = express()
+const appRouter = router({
+
+})
 
 app.use(morgan("dev"))
+
+app.use("/trpc", trpcExpress.createExpressMiddleware({
+  router: appRouter,
+  createContext
+}))
 
 export default app
---
+++ src/trpc.ts
@@ -0,0 +1,12 @@
+import { initTRPC } from "@trpc/server"
+import type * as trpcExpress from "@trpc/server/adapters/express"
+
+export const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
+
+})
+
+const t = initTRPC.context().create()
+
+export const router = t.router
+export const middleware = t.middleware
+export const publicProcedure = t.procedure
