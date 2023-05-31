b49b47a9873a5791e5ead6826683c0673c283b7c
nkwaaaa <nkwaaaa@gmail.com>
2023-05-31 11:28:15 -0300

    add cors and notes route

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,22 @@ import express from "express"
 import morgan from "morgan"
 import * as trpcExpress from "@trpc/server/adapters/express"
 import { router, createContext } from "./trpc"
+import { notesRouter } from "./routes/notes"
+import cors from "cors"
 
 const app = express()
 const appRouter = router({
-
+  note: notesRouter
 })
 
+app.use(cors())
 app.use(morgan("dev"))
 
 app.use("/trpc", trpcExpress.createExpressMiddleware({
   router: appRouter,
   createContext
 }))
+
+export type AppRouter = typeof appRouter
 
 export default app
